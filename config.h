74a2c1657177b17e8202b40163b84147b83afa53
GarytheNoob_on_archlinux <garythenoob@qq.com>
2023-06-22 23:39:07 +0800

    add .gitignore

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font =  "JetBrainsMono Nerd Font Mono:size=8";
+static char *font =  "JetBrainsMono Nerd Font Mono:size=8:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
